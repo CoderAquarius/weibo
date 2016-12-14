5af2945679dd025aff3393811d99067d3874fe57
Aquarius <1264727563@qq.com>
2016-12-14 17:07:30 +0800

    创建了person类

---
+++ weibo/weibo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weibo
+//
+//  Created by jiangjunfu on 16/12/14.
+//  Copyright © 2016年 jiangjunfu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weibo/weibo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weibo
+//
+//  Created by jiangjunfu on 16/12/14.
+//  Copyright © 2016年 jiangjunfu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
