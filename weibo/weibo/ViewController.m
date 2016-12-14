81b599c944a4e7c57beecf342465b8ab22d27725
Aquarius <1264727563@qq.com>
2016-12-14 17:04:38 +0800

    开始开发

--- weibo/weibo/ViewController.m
+++ weibo/weibo/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"%s", __FUNCTION__);
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
