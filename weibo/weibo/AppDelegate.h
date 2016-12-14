156d68abd6043da668768d06ca6baf0c44b4a952
Aquarius <1264727563@qq.com>
2016-12-14 16:43:53 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by jiangjunfu on 16/12/14.
+//  Copyright © 2016年 jiangjunfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by jiangjunfu on 16/12/14.
+//  Copyright © 2016年 jiangjunfu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
